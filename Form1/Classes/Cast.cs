83835ced92509bc30b0b19fd42891572369acd1a
Arseniy Yuhnovets <closserd@mail.ru>
2025-03-04 11:56:30 +0300

    feat: Add full functional

---
+++ Form1/Classes/Cast.cs
@@ -0,0 +1,12 @@
+﻿using WinFormsApp2.Interfaces;
+using Newtonsoft.Json.Linq;
+
+namespace WinFormsApp2.Classes;
+
+public class Cast : ICast
+{
+    public JObject GetJObject(string json)
+    {
+        return JObject.Parse(json);
+    }
+}
---
+++ Form1/Classes/GetFileText.cs
@@ -0,0 +1,26 @@
+﻿using WinFormsApp2.Interfaces;
+
+namespace WinFormsApp2.Classes;
+
+public class GetFileText: IGetFileText
+{
+    //переменная для названия переведенного файла
+    public string fileName { get; set; } = null;
+    //открытие файла и копирование исходного текста для перевода,
+    //присвоение переменной fileName имени открытого файла
+    public string GetText()
+    {
+        fileName = null;
+        var fileText = string.Empty;
+        OpenFileDialog openFile = new OpenFileDialog();
+            
+        if (openFile.ShowDialog() != DialogResult.OK)
+        {
+            throw new Exception("Выберите файл.");
+        }
+        fileText = File.ReadAllText(openFile.FileName);
+        fileName = Path.GetFileNameWithoutExtension(openFile.FileName);
+                    
+        return fileText;
+    }
+}
